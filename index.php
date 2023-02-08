f0582d149304227fe078c3132707e0383d047b86
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2023-02-09 00:03:24 +0100

    Customize Homepage

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 
 <head>
   <meta charset="UTF-8">
@@ -19,7 +19,7 @@
   <div class="container">
     <header
       class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+      <a href="index.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
         <img src="assets/images/logo-cuisinea-horizontal.jpg" alt="Logo Cuisinea" width="250">
       </a>
 
@@ -43,24 +43,25 @@
           loading="lazy">
       </div>
       <div class="col-lg-6">
-        <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
-        <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most
-          popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive
-          prebuilt components, and powerful JavaScript plugins.</p>
+        <h1 class="display-5 fw-bold lh-1 mb-3">Cuisinea - Recettes de cuisine</h1>
+        <p class="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, aperiam, consequatur quos
+          voluptatem voluptatum, suscipit unde officia vel consectetur recusandae natus est? Veritatis aliquam eligendi
+          saepe repellat ratione provident eius?</p>
         <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-          <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Voir nos recettes</button>
         </div>
       </div>
     </div>
 
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-          content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="row">
+      <div class="card" style="width: 18rem;">
+        <img src="uploads/recipes/1-chocolate-au-mousse.jpg" class="card-img-top" alt="Mousse au chocolat">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
     </div>
 
