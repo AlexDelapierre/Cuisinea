19518da22bd8fb71fd6f4e4493161ae98ac7ac8c
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2023-07-10 19:40:03 +0200

    Add config.php and recipe.php in lib folder

--- recettes.php
+++ recettes.php
@@ -1,13 +1,6 @@
 <?php
-  define('_RECIPES_IMG_PATH_', 'uploads/recipes/'); 
-  
-  $recipes = [
-    ['title' => 'Mousse au chocolat', 'description' => 'Mousse quick example text to build on the card title and make up the bulk of the card\'s content.', 'image' => '1-chocolate-au-mousse.jpg'],
-    ['title' => 'Gratin dauphinois', 'description' => 'Gratin quick example text to build on the card title and make up the bulk of the card\'s content.', 'image' => '2-gratin-dauphinois.jpg'],
-    ['title' => 'Salade de chèvre', 'description' => 'Salade quick example text to build on the card title and make up the bulk of the card\'s content.', 'image' => '3-salade.jpg'],    
-  ];
-
   require_once('templates/header.php');
+  require_once('lib/recipe.php');
 ?>
 
 <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
