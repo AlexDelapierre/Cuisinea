a3f163ee52b77eec42010b1920b92cb411b06a35
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2023-07-13 20:48:25 +0200

    Add CRUD form and SQL INSERT request

--- lib/config.php
+++ lib/config.php
@@ -3,8 +3,10 @@
   define('_ASSETS_IMG_PATH_', 'assets/images/'); 
   define('_HOME_RECIPES_LIMIT', 6); 
 
+  // Variable contenant un tableau contenant les onglets du menu du header
   $mainMenu = [
     'index.php' => 'Accueil',
     'recettes.php' =>'Nos recettes',
+    'ajout_modification_recette.php' => 'Ajout/modif recette'
    ];
   
