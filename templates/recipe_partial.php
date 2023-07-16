4d77065f601fac405eedae77dd2623d9af481233
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2023-07-16 19:35:58 +0200

    Add wireframes in annexes folder

--- templates/recipe_partial.php
+++ templates/recipe_partial.php
@@ -2,7 +2,7 @@
   <div class="card">
     <img src="<?=getRecipeImage($recipe['image']); ?>" class="card-img-top" alt="<?= $recipe['title']; ?>">
     <div class="card-body">
-      <h5 class="card-title"><?= $recipe['title']; ?></h5>
+      <h2 class="card-title"><?= $recipe['title']; ?></h2>
       <p class="card-text"><?= $recipe['description']; ?></p>
       <a href="recette.php?id=<?= $recipe['id']; ?>" class="btn btn-primary">Voir la recette</a>
     </div>
