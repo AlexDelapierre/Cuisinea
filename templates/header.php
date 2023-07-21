f33d9d878a66d26a53d2b2d7fcb94dda40d3c19e
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2023-07-21 21:25:39 +0200

    Add SQL file

--- templates/header.php
+++ templates/header.php
@@ -25,7 +25,7 @@
 
 <body>
 
-  <div class="container-fluid">
+  <div class="container">
     <header
       class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
       <a href="index.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
